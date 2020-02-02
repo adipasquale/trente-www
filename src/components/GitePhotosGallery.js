60481fd3026a22a036d2d61bdb0bb7f96d390d2b
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-02-02 19:55:22 +0100

    added zoom on photo gallery

--- src/components/GitePhotosGallery.js
+++ src/components/GitePhotosGallery.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Box, Image } from '@chakra-ui/core'
+import React, { useState } from 'react'
+import { Box, Image, useDisclosure, Button, Modal, ModalOverlay, ModalContent, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/core'
 
 import p1 from '../images/gite/gite1.jpg'
 import p2 from '../images/gite/gite2.jpg'
@@ -9,22 +9,68 @@ import p5 from '../images/gite/gite5.jpg'
 import p6 from '../images/gite/gite6.jpg'
 const photos = [p1, p2, p3, p4, p4, p5, p6]
 
-const GitePhotosGallery = () => (
-  <Box display='flex' overflow='scroll' paddingBottom={3}>
-    {photos.map((photo, idx) =>
-      <Box
-        key={idx}
-        flexShrink={0}
-        width={250}
-        border='1px solid #ccc'
-        rounded='lg'
-        overflow='hidden'
-        marginLeft={idx > 0 ? 2 : 0}
-      >
-        <Image objectFit='cover' maxHeight={250} src={photo} />
+const PhotoModal = ({ isOpen, onClose }) => {
+  return (
+    <>
+    </>
+  )
+}
+
+const GitePhotosGallery = () => {
+  const { isOpen: isModalOpen, onOpen: onModalOpen, onClose: onModalClose } = useDisclosure()
+  const [displayedPhoto, setDisplayedPhoto] = useState()
+
+  return (
+    <>
+      <Box display='flex' overflowY='scroll' paddingBottom={3}>
+        {photos.map((photo, idx) =>
+          <Box
+            key={idx}
+            flexShrink={0}
+            width={250}
+            height={250}
+            rounded='lg'
+            overflow='hidden'
+            marginLeft={idx > 0 ? 2 : 0}
+            p={0}
+          >
+            <Image
+              width='100%'
+              height='100%'
+              objectFit='cover'
+              src={photo}
+              m={0}
+              onClick={(e) => {
+                setDisplayedPhoto(photo)
+                onModalOpen()
+              }}
+            />
+          </Box>
+        )}
       </Box>
-    )}
-  </Box>
-)
+      <Modal
+        isCentered
+        maxWidth='80vh'
+        size='4xl'
+        isOpen={isModalOpen}
+        onClose={onModalClose}
+      >
+        <ModalOverlay />
+        <ModalContent p={3} backgroundColor='rgba(255,255,255, 0.5)'>
+          <ModalCloseButton />
+          <ModalBody>
+            <Box textAlign='center'>
+              <Image
+                src={displayedPhoto}
+                maxWidth='100%'
+                maxHeight='80vh'
+              />
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
 
 export default GitePhotosGallery
